1d19337bc8e93f4ac54c632d55b32af04f2787ea
Sarun Intaralawan <sarun.intaralawan@ivao.aero>
2021-11-18 09:01:35 +0700

    feat: use Winston logger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { WinstonModule } from 'nest-winston';
 import { resolve } from 'path';
+import { format, transports } from 'winston';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: WinstonModule.createLogger({
+      format: format.json(),
+      transports: [
+        new transports.Console(),
+        new transports.File({
+          filename: resolve(__dirname, '..', 'log', 'log.txt')
+        })
+      ],
+      exitOnError: false
+    })
+  });
   app.useStaticAssets(resolve(__dirname, '..', 'public'));
   app.setBaseViewsDir(resolve(__dirname, 'views'));
   app.setViewEngine('ejs');
