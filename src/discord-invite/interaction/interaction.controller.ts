526165b85e44aa50e5f69ee30c0f96d72db20c63
Sarun Intaralawan <sarun.intaralawan@ivao.aero>
2025-03-09 00:36:02 +0700

    feat(InteractionController): implement basic interaction handling

--- src/discord-invite/interaction/interaction.controller.ts
+++ src/discord-invite/interaction/interaction.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Headers,
+  Inject,
+  Post,
+  RawBodyRequest,
+  Request,
+  UnauthorizedException
+} from '@nestjs/common';
+import {
+  APIInteraction,
+  InteractionType,
+  InteractionResponseType,
+  APIInteractionResponse,
+  MessageFlags
+} from 'discord-api-types/v10';
+import { Request as ExpressRequest } from 'express';
+import { crypto_sign_verify_detached } from 'libsodium-wrappers';
 
-@Controller('interaction')
-export class InteractionController {}
+@Controller('discord/interaction')
+export class InteractionController {
+  @Post()
+  receiveInteraction(
+    @Body() interaction: APIInteraction,
+    @Request() request: RawBodyRequest<ExpressRequest>,
+    @Headers('X-Signature-Ed25519') signature: string,
+    @Headers('X-Signature-Timestamp') timestamp: string,
+    @Inject('DISCORD_BOT_PUBLIC_KEY') publicKey: string
+  ): APIInteractionResponse {
+    if (
+      crypto_sign_verify_detached(
+        Buffer.from(signature, 'utf8'),
+        Buffer.concat([Buffer.from(timestamp, 'utf8'), request.rawBody || Buffer.from([])]),
+        Buffer.from(publicKey, 'utf8')
+      )
+    ) {
+      if (interaction.type === InteractionType.Ping) {
+        return {
+          type: InteractionResponseType.Pong
+        };
+      }
+      return {
+        type: InteractionResponseType.ChannelMessageWithSource,
+        data: {
+          content: 'UnhandledInteractionException',
+          flags: MessageFlags.Ephemeral
+        }
+      };
+    } else {
+      throw UnauthorizedException;
+    }
+  }
+}
